c6d3719cfed1f62bf94dd67fbc52ffdd4ea8467f
soumyamca <soumyamoolamannil@gmail.com>
2022-11-25 12:04:02 +0530

    project complete

--- app.js
+++ app.js
@@ -3,14 +3,17 @@ var bodyparser = require('body-parser')
 var app = express();
 app.use(bodyparser.urlencoded({extended:false}))
 app.use(bodyparser.json())
+app.get('/',(req,res)=>{
+    res.send("welcome to my website")
+})
 app.post('/read',(req,res)=>{
-    var getName=req.body.name ;
+    var getName=req.body.bookname;
     var getCategory=req.body.cat;
     var getAuthor=req.body.author;
     var getPrice=req.body.price;
 
-    res.send(getName+"",getCategory+"",getAuthor+"",getPrice)
+    res.json({"name":getName,"category":getCategory,"author":getAuthor,"price":getPrice})
 })
-app.listen(3005,()=>{
-    console.log("server started at http://localhost:3005/read")
+app.listen(3010,()=>{
+    console.log("server started at http://localhost:3010/read")
 })
